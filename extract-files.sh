de0fb37929834247eb39103f4b66f7d9bed610a5
Arian <arian.kulmer@web.de>
2022-06-22 16:31:14 +0200

    cupid: Bring back face detection libs
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -27,6 +27,12 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_18}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
+        vendor/lib64/hw/camera.qcom.so | vendor/lib64/libFaceDetectpp-0.5.2.so | vendor/lib64/libfacedet.so)
+            [ "$2" = "" ] && return 0
+            sed -i "s|libmegface.so|libfacedet.so|g" "${2}"
+            sed -i "s|libMegviiFacepp-0.5.2.so|libFaceDetectpp-0.5.2.so|g" "${2}"
+            sed -i "s|megviifacepp_0_5_2_model|facedetectpp_0_5_2_model|g" "${2}"
+            ;;
         *)
             return 1
             ;;
